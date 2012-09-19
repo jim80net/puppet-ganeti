5e26d232ae011c6d1781ff38316d49f7ae8de00a
Jim Park <jpark@jim80.net>
2012-09-19 15:42:29 -0500

    Test case added for dev scratch node "testpup-6-0"

---
+++ tests/nodes/testpup6-0.pp
@@ -0,0 +1,20 @@
+##
+# This test will ensure that the ganeti vg is in place, then install ganeti. 
+# This test will NOT initialize the cluster. 
+
+import "lvm"
+
+physical_volume {
+  "/dev/vdb":
+    ensure => present;
+  "/dev/vdc":
+    ensure => present;
+}
+
+volume_group { 
+  "ganeti":
+    ensure => present,
+    physical_volumes => ["/dev/vdb", "/dev/vdc"]
+}
+
+include ganeti::redhat::ganeti::install
